f7250eabf655dfebda6d04567f8a9655d65967cc
takeyy <t0k6yist@gmail.com>
2021-04-30 02:08:12 +0900

    共起ネットワーク可視化テスト実装（怪人二十面相）

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 from flask import Flask, render_template, request, redirect, url_for
+import numpy as np
+import pandas as pd
+import itertools
 
 app = Flask(__name__)
 
 
+def get_hinshi_dict():
+    return {'all': '全て', 'meishi': '名詞', 'keiyoushi': '形容詞', 'fukushi': '副詞'}
+
+
+def get_edogawa_df():
+    return pd.read_csv('csv/edogawa_list.csv')
+
+
 @app.route('/')
-def hello():
-    name = 'テストユーザー'
-    return render_template('index.html', title='test', name=name)
+def index():
+    title = '江戸川乱歩作品の共起ネットワーク可視化システム'
+    return render_template('index.html', title=title, hinshi_dict=get_hinshi_dict(), edogawa_df=get_edogawa_df())
 
 
-@app.route('/post', methods=['GET', 'POST'])
-def post():
+@app.route('/network_visualization', methods=['GET', 'POST'])
+def network_visualization():
     if request.method == 'POST':
         name = request.form['name']
-        return render_template('index.html', name=name)
+        hinshi = request.form['hinshi']
+        number = request.form['number']
+        try:
+            return render_template(f'network/{name}_{hinshi}_{number}.html')
+        except:
+            return redirect(url_for('index'))
     else:
         # エラーなどでリダイレクトしたい場合
         return redirect(url_for('index'))
