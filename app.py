6e8aa7f5d3136e919c88f63d19179714ac61f21e
takeyy <t0k6yist@gmail.com>
2021-05-07 23:13:14 +0900

    共起ネットワークをiframeで表示

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 import numpy as np
 import pandas as pd
@@ -23,11 +24,14 @@ def index():
 @app.route('/network_visualization', methods=['GET', 'POST'])
 def network_visualization():
     if request.method == 'POST':
+        title = '江戸川乱歩作品の共起ネットワーク可視化システム'
         name = request.form['name']
         hinshi = request.form['hinshi']
         number = request.form['number']
+        EXTERNAL_STATIC_FILE_PATH = os.environ.get('EXTERNAL_STATIC_FILE_PATH')
+        request_path = f'{EXTERNAL_STATIC_FILE_PATH}/{name}_{hinshi}_{number}.html'
         try:
-            return render_template(f'network/{name}_{hinshi}_{number}.html')
+            return render_template('index.html', title=title, hinshi_dict=get_hinshi_dict(), edogawa_df=get_edogawa_df(), request_path=request_path)
         except:
             return redirect(url_for('index'))
     else:
