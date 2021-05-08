45b4e23f0c9a3fc2a3434be3a9b4c8374fcfe0e9
takeyy <t0k6yist@gmail.com>
2021-05-08 22:02:15 +0900

    headerの修正

--- app.py
+++ app.py
@@ -18,19 +18,22 @@ def get_edogawa_df():
 @app.route('/')
 def index():
     title = 'ホーム'
-    return render_template('index.html', title=title)
+    active_url = 'home'
+    return render_template('index.html', title=title, active_url=active_url)
 
 
 @app.route('/co-occurrence_network')
 def co_occurrence_network():
     title = '共起ネットワーク'
-    return render_template('co-occurrence_network.html', title=title, hinshi_dict=get_hinshi_dict(), edogawa_df=get_edogawa_df())
+    active_url = 'co-oc_network'
+    return render_template('co-occurrence_network.html', title=title, active_url=active_url, hinshi_dict=get_hinshi_dict(), edogawa_df=get_edogawa_df())
 
 
 @app.route('/co-occurrence_network/visualization', methods=['GET', 'POST'])
 def network_visualization():
     if request.method == 'POST':
         title = '共起ネットワーク'
+        active_url = 'co-oc_network'
         name, file_name = request.form['name'].split('-')
         hinshi_eng, hinshi_jpn = request.form['hinshi'].split('-')
         number = 250 if hinshi_jpn in ['名詞', '動詞', '形容詞', '副詞'] else 1000
@@ -38,7 +41,7 @@ def network_visualization():
         request_path = f'{EXTERNAL_STATIC_FILE_PATH}/{file_name}_{hinshi_eng}_{number}.html'
         request_data = {'name': name, 'hinshi': hinshi_jpn, 'number': number}
         try:
-            return render_template('co-occurrence_network.html', title=title, hinshi_dict=get_hinshi_dict(), edogawa_df=get_edogawa_df(), request_path=request_path, request_data=request_data)
+            return render_template('co-occurrence_network.html', title=title, active_url=active_url, hinshi_dict=get_hinshi_dict(), edogawa_df=get_edogawa_df(), request_path=request_path, request_data=request_data)
         except:
             return redirect(url_for('co_occurrence_network'))
     else:
