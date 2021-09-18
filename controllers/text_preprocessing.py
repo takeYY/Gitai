99ca48699376cd5a4a8b9b46506c274abbabb55a
takeyy <t0k6yist@gmail.com>
2021-09-18 23:55:01 +0900

    テキストの前処理：送るデータを事前に集約

--- controllers/text_preprocessing.py
+++ controllers/text_preprocessing.py
@@ -22,30 +22,30 @@ def show():
 
     if request.method == 'GET':
         return render_template('preprocessing.html', basic_data=basic_data, other_option=other_option, description=other_option_description)
-    else:
-        # 利用者から送られてきた情報を取得
-        texts = request.form.get('texts')
-        # textsがない場合
-        if not texts:
-            flash('テキストが入力されていません。', 'error')
-            return render_template('preprocessing.html', basic_data=basic_data, other_option=other_option, description=other_option_description)
-        remove_words = request.form['remove-texts']
-        remove_word_in_texts = request.form['remove-text-in-texts']
-        replace_words = request.form['replace-texts']
-        other_options = request.form.getlist('other-option')
-        # 選択されたその他設定を取得
-        selected_option = [other_option.get(k) for k in other_options]
-        # 送られてきた情報を基にテキストを前処理
-        preprocessed_text, errors = texts_preprocessing(
-            texts, remove_words, remove_word_in_texts, replace_words, other_options)
-        # エラーがある場合
-        if errors:
-            for error in errors:
-                flash(error, 'error')
-            sent_data = dict(texts=texts, remove_texts=remove_words, remove_text_in_texts=remove_word_in_texts,
-                             other_option=selected_option, replace_texts=replace_words)
-            return render_template('preprocessing.html', basic_data=basic_data, sent_data=sent_data, other_option=other_option, description=other_option_description)
-        # 送るデータをまとめる
-        sent_data = dict(texts=texts, remove_texts=remove_words, remove_text_in_texts=remove_word_in_texts,
-                         other_option=selected_option, replace_texts=replace_words, preprocessed_texts=preprocessed_text)
-        return render_template('preprocessing.html', basic_data=basic_data, sent_data=sent_data, other_option=other_option, description=other_option_description)
+
+    # 利用者から送られてきた情報を取得
+    texts = request.form.get('texts')
+    # textsがない場合
+    if not texts:
+        flash('テキストが入力されていません。', 'error')
+        return render_template('preprocessing.html', basic_data=basic_data, other_option=other_option, description=other_option_description)
+    remove_words = request.form['remove-texts']
+    remove_word_in_texts = request.form['remove-text-in-texts']
+    replace_words = request.form['replace-texts']
+    other_options = request.form.getlist('other-option')
+    # 選択されたその他設定を取得
+    selected_option = [other_option.get(k) for k in other_options]
+    # 送られてきた情報を基にテキストを前処理
+    preprocessed_text, errors = texts_preprocessing(
+        texts, remove_words, remove_word_in_texts, replace_words, other_options)
+    # 送るデータの辞書
+    sent_data_dict = dict(texts=texts, remove_texts=remove_words, remove_text_in_texts=remove_word_in_texts,
+                          other_option=selected_option, replace_texts=replace_words)
+    # エラーがある場合
+    if errors:
+        for error in errors:
+            flash(error, 'error')
+        return render_template('preprocessing.html', basic_data=basic_data, sent_data=sent_data_dict, other_option=other_option, description=other_option_description)
+    # 送るデータをまとめる
+    sent_data_dict['preprocessed_texts'] = preprocessed_text
+    return render_template('preprocessing.html', basic_data=basic_data, sent_data=sent_data_dict, other_option=other_option, description=other_option_description)
