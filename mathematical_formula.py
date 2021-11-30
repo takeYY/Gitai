5fdf89922dd409c8a6bb073af094c14436cb8c8a
takeyy <t0k6yist@gmail.com>
2021-11-30 20:25:32 +0900

    数式の定義説明を追加

--- mathematical_formula.py
+++ mathematical_formula.py
@@ -1,6 +1,6 @@
 def get_count_formula():
     formula = r"""\begin{align}
-      共起回数=|A\cap B|
+      共起頻度=|A\cap B|
       \end{align}"""
     return formula
 
@@ -30,4 +30,18 @@ def get_pmi_formula():
     formula = r"""\begin{align}
       相互情報量=\log_2\frac{|A\cap B|\times N}{|A|\times|B|}
       \end{align}"""
+    return formula
+
+
+def get_co_oc_formula_description():
+    formula = r"""\begin{align}
+      A&：単語aの集合\\
+      B&：単語bの集合\\
+      |A\cup B|&：AとBの和集合の要素数\\
+      |A|&：Aの要素数\\
+      |B|&：Bの要素数\\
+      min(|A|,|B|)&：AとBの要素数の中から最小値\\
+      N&：総語数
+    \end{align}"""
+
     return formula
